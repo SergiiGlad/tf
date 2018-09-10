2bd80f98c11803ea9ec2f6be81a1b4a112b7bcf3
sergiiglad <sergii.gladchenko@gmail.com>
2018-09-10 14:40:08 +0300

    Update settings.py

--- rocket/rocket/settings.py
+++ rocket/rocket/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'so&7g%c+8628%ous&w1kmav&+%88c@^nph_8odj=t+(mh75@e)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
