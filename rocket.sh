d8998ac50f688d4a051146f2859dcf7652241f01
sergiiglad <sergii.gladchenko@gmail.com>
2018-09-10 12:40:44 +0300

    Update rocket.sh

--- rocket.sh
+++ rocket.sh
@@ -5,4 +5,12 @@
 
 sudo yum -y install python python-pip
 pip install --user django
+
+#
+# Get info on current EC2 Instance
+#
+curl http://169.254.169.254/latest/meta-data/public-ipv4
+curl http://169.254.169.254/latest/meta-data/public-hostname
+
+# Start django
 python ./tf/rocket/manage.py runserver 0:8000 
