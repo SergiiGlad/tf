a5b1ac23bd7ec60cf606f59d668f7a66b8064f2e
sergiiglad <sergii.gladchenko@gmail.com>
2018-09-10 12:45:46 +0300

    Delete views.py

--- rocket/rocket/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-# Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("<h1> HELLO ALL !!!</h1> <br> <h2> Demo 2 is comming SOON!")
